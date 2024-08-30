29bb6471e9ba60e71cdc6ade2e71830086b06c8a
Mohamed <m.hamiche99@gmail.com>
2024-08-30 10:03:20 +0200

    add requirements.txt

--- data_producer_user_data.sh
+++ data_producer_user_data.sh
@@ -16,7 +16,7 @@ echo 'export CLONE_DIR="${CLONE_DIR}"' >> /home/ec2-user/.bash_profile
 echo 'export BOOTSTRAP_SERVERS="${BOOTSTRAP_SERVERS}"' >> /home/ec2-user/.bash_profile
 echo 'export SECURITY_PROTOCOL="${SECURITY_PROTOCOL}"' >> /home/ec2-user/.bash_profile
 
-source /home/ec2-user/.bash_profile
+# source /home/ec2-user/.bash_profile
 
 # Clone the GitHub repository
 git clone "${GITHUB_REPO_URL}" "${CLONE_DIR}"
@@ -24,13 +24,10 @@ git clone "${GITHUB_REPO_URL}" "${CLONE_DIR}"
 # Navigate to the cloned repository directory
 cd "${CLONE_DIR}" || exit
 
-git checkout develop # debug
+git checkout develop # dev
 
-# Make sure the package is executable
-chmod +x confluent_kafka_package/*.whl
-
-# Install the confluent-kafka package from the wheel file
-pip3 install confluent_kafka_package/*.whl
+# Install requirements
+pip3 install -r requirements.txt
 
 # Ensure the script is executable
 chmod +x code/data_producer.py
@@ -39,6 +36,6 @@ chmod +x code/data_producer.py
 python3 code/data_producer.py "${BOOTSTRAP_SERVERS}" "${SECURITY_PROTOCOL}" "${SKYSCANNER_API_KEY}"
 
 # Add cron jobs to the crontab for the ec2-user
-# Run every minute and at reboot, passing the environment variables as arguments
-(crontab -l 2>/dev/null; echo "* * * * * /usr/bin/python3 "${CLONE_DIR}"/code/data_producer.py \"${BOOTSTRAP_SERVERS}\" \"${SECURITY_PROTOCOL}\" \"${SKYSCANNER_API_KEY}\"  >> /home/ec2-user/data_producer.log 2>&1") | crontab -
+# Run every x minutes and at reboot, passing the environment variables as arguments
+(crontab -l 2>/dev/null; echo "*/3 * * * * /usr/bin/python3 "${CLONE_DIR}"/code/data_producer.py \"${BOOTSTRAP_SERVERS}\" \"${SECURITY_PROTOCOL}\" \"${SKYSCANNER_API_KEY}\"  >> /home/ec2-user/data_producer.log 2>&1") | crontab -
 (crontab -l 2>/dev/null; echo "@reboot /usr/bin/python3 "${CLONE_DIR}"/code/data_producer.py \"${BOOTSTRAP_SERVERS}\" \"${SECURITY_PROTOCOL}\" \"${SKYSCANNER_API_KEY}\" >> /home/ec2-user/data_producer.log 2>&1") | crontab -
